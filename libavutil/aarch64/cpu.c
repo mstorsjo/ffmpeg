1be308197c027401424feba424c55b90b69aa947
Martin Storsjö <martin@martin.st>
2023-05-25 15:47:15 +0300

    aarch64: Add linux runtime cpu feature detection using getauxval(AT_HWCAP)
    Based on code by Janne Grunau.
    
    Using HWCAP_CPUID for user space access to the CPU feature registers. See
    https://www.kernel.org/doc/html/latest/arm64/cpu-feature-registers.html.

--- libavutil/aarch64/cpu.c
+++ libavutil/aarch64/cpu.c
@@ -20,6 +20,42 @@
 #include "libavutil/cpu_internal.h"
 #include "config.h"
 
+#if (defined(__linux__) || defined(__ANDROID__)) && HAVE_GETAUXVAL && HAVE_ASM_HWCAP_H
+#include <stdint.h>
+#include <asm/hwcap.h>
+#include <sys/auxv.h>
+
+#define get_cpu_feature_reg(reg, val) \
+        __asm__("mrs %0, " #reg : "=r" (val))
+
+static int detect_flags(void)
+{
+    unsigned long ret = getauxval(AT_HWCAP);
+    int flags = 0;
+#if defined(HWCAP_CPUID)
+    uint64_t tmp;
+    if (!(ret & HWCAP_CPUID))
+        return flags;
+    get_cpu_feature_reg(ID_AA64ISAR0_EL1, tmp);
+    if (((tmp >> 44) & 0xf) == 0x1)
+        flags |= AV_CPU_FLAG_DOTPROD;
+    get_cpu_feature_reg(ID_AA64ISAR1_EL1, tmp);
+    if (((tmp >> 52) & 0xf) == 0x1)
+        flags |= AV_CPU_FLAG_I8MM;
+#endif
+
+    return flags;
+}
+
+#else
+
+static int detect_flags(void)
+{
+    return 0;
+}
+
+#endif
+
 int ff_get_cpu_flags_aarch64(void)
 {
     int flags = AV_CPU_FLAG_ARMV8 * HAVE_ARMV8 |
@@ -32,6 +68,8 @@ int ff_get_cpu_flags_aarch64(void)
 #ifdef __ARM_FEATURE_I8MM
     flags |= AV_CPU_FLAG_I8MM;
 #endif
+
+    flags |= detect_flags();
 
     return flags;
 }
